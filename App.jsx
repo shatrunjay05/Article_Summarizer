4d05541918e8375919af4e928d77f575fff9055e
Shatrunjay Kumar <shatrunjay2305@gmail.com>
2023-09-12 21:43:56 +0530

    new commit

---
+++ App.jsx
@@ -0,0 +1,22 @@
+
+import Hero from './componenets/Hero';
+import Demo from './componenets/Demo';
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+      <div className='main'>
+        <div className='gradient'></div>
+      </div>
+
+      <div className='app'>
+        <Hero/>
+        <Demo/>
+      </div>
+
+    </main>
+  )
+}
+
+export default App
